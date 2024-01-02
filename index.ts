b1a44000142a09aeb7d0663590d9807db0304280
mitchell <mitchellkim94@icloud.com>
2024-01-02 21:38:00 +0900

    feat: command

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
+import { Command } from 'commander'
+
 function createComponent() {
     
 }
+
+const program = new Command()
+
+program
+    .argument('<componentName>', 'Component name')
+    .action((componentName) => {
+        console.log('생성하려는 컴포넌트', componentName);
+    })
+
+program.parse(process.argv)
