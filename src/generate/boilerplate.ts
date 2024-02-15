cb586d17bc882c4fba743ffc8926bf96031fb6b2
mitchell <mitchellkim94@icloud.com>
2024-02-15 23:33:00 +0900

    refactor: make boilerplate files by text file

---
+++ src/generate/boilerplate.ts
@@ -0,0 +1,32 @@
+import fs from 'fs-extra'
+import path from 'path'
+
+type BoilerPlateContents =
+  | 'index.txt'
+  | 'index.txt'
+  | 'NAME_REPLACED.jsx'
+  | 'NAME_REPLACED.stories.jsx'
+  | 'NAME_REPLACED.test.jsx'
+  | 'NAME_REPLACED.txt'
+  | 'NAME_REPLACED.stories.txt'
+  | 'NAME_REPLACED.test.txt'
+
+const IDENTIFIER = 'NAME_REPLACED'
+
+export function makeBoilerPlate(
+  path: string,
+  name: string,
+  identifier = IDENTIFIER,
+) {
+  const contents = fs.readFileSync(path, 'utf-8')
+  return contents.replaceAll(identifier, name)
+}
+
+export function getBoilerPlates(
+  fileName: BoilerPlateContents,
+  replaceName: string,
+  ext: 'js' | 'ts' = 'ts',
+) {
+  const contentsPath = path.join(__dirname, `../contents/${ext}/${fileName}`)
+  return makeBoilerPlate(contentsPath, replaceName)
+}
