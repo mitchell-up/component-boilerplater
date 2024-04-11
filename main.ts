1034d50bbbfdf497789a968992513871a4f55b63
mitchell <mitchellkim94@icloud.com>
2024-04-11 23:22:00 +0900

    feat: cli option added

--- main.ts
+++ main.ts
@@ -1,17 +1,31 @@
 import { Command } from 'commander'
 import { readConfig } from './src/config/readConfig'
 import { generateBoilerPlates } from './src/generate/generateBoilerPlates'
+import packageJson from './package.json'
 
 const CONFIG_FILENAME = 'boilerplate.config.json'
 
 const program = new Command()
 
 program
+  .name(packageJson.name)
+  .description(packageJson.description)
+  .version(packageJson.version)
+
+program
   .argument('<componentName>', 'Component name')
-  .action((componentName) => {
+  .option('--js', 'Use JavaScript Extentions.')
+  .option('-d, --dir <path>', 'Output directory the files will be generated.')
+  .action((componentName, options) => {
     const configs = readConfig(CONFIG_FILENAME)
+
+    if (options.js) {
+      configs.ext = 'js'
+    }
+
+    if (options.dir) {
+      configs.baseDir = options.dir
+    }
 
     generateBoilerPlates(componentName, configs)
   })
-
-program.parse(process.argv)
